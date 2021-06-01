d59dc045e4dd8ffe91d619c1983b30725e5a3a1d
dominicklyons <85123874+dominicklyons@users.noreply.github.com>
2021-06-01 21:01:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
-const WebSocket = require('ws');
-const port = process.env.PORT || 8989;
-const wss = new WebSocket.Server({ port });
-wss.on('connection', function connection(ws, req) {
-	const ip = req.socket.remoteAddress;
-	const id = req.headers['sec-websocket-key'];
+'use strict'; // enables strict mode that will throw more exceptions for errors
 
+const express = require('express'); // imports a web framework for nodejs
+const WebSocket = require('ws'); // imports the websocket framework
+const SocketServer = require('ws').Server; // imports the websocket server framework
 
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-    client.send('"message":"'+data+'"');
-      }
+const PORT = process.env.PORT || 5500;
+/*
+Picks the port that the server will run on.
+process.env.PORT is a runtime variable that can be set on Heroku.
+However, when deploying on Heroku, the port seems to be ignored.
+*/
+
+const server = express()
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// creates the server and logs the port it's running on
+
+const wss = new SocketServer({ server });
+// creates the websocket server
+
+wss.on('connection', (ws) => { // when the socket server has a connection, this function is ran. ws represents the client.
+    console.log('Client connected');
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send('{"","message":"' + data + '"}');
+            }
+        });
     });
-  });
-    ws.on('close', function incoming(data)  {
-         wss.clients.forEach(function each(client) {
-         if (client.readyState === WebSocket.OPEN) {
-           client.send('"message":"closed"');
-      }
-    });
-    });
+    ws.on('close', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send('{"","message":"closed"}');
+            }
+        });
+    }); // once the client leaves, this function is ran. all that is done right now is to log that they have disconnected.
 });
+
+
+/* the code below has the server essentially ping itself.
+herokuapp puts apps to sleep after 30 minutes of inactivity,
+and will reboot once a request is received. This takes time, usually around 20 seconds-ish.
+20 second downtime is unacceptable for someone wanting to play, so thus this keepawake function is ran.*/
+const enableKeepAlive = process.env.KEEP_ALIVE || false;
+if (enableKeepAlive) {
+    const http = require("http");
+    setInterval(function() {
+        http.get(process.env.URL);
+    }, 100 * 60 * 5); // every 5 minutes
+}
